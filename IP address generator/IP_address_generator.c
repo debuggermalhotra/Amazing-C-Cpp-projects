8c50fecc092c467ff6b8f70556b1310f1285433f
debuggermalhotra <chetanalwaysmalhotra@hotmail.com>
2016-07-07 02:16:37 +0530

    Random IP generator(Class wise)

---
+++ IP address generator/IP_address_generator.c
@@ -0,0 +1,58 @@
+//This program currently returns the IP address as an integer which can't be parsed. 
+//Would be commiting soon with this program returning IP as string.
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+
+
+void IPaddressgenerator(char IPclass)
+{
+	int octet1;
+	srand(time(NULL));   //srand sets the seed of rand
+    if(IPclass=='A'|| IPclass=='a')
+    octet1 = 0 + (rand() % ( 127 - 0 + 1 ) );
+   
+   else if(IPclass=='B' || IPclass=='b' )
+   octet1 = 128 + (rand() % ( 191 - 128 + 1 ) );
+   
+   else if(IPclass=='C' || IPclass=='c')
+   octet1 = 192 + (rand() % ( 192 - 223 + 1 ));
+   
+   else if(IPclass=='D' || IPclass=='d')
+   octet1 = 224 + (rand() % ( 224 - 239 + 1 ));
+   
+   else if(IPclass=='E' || IPclass=='e')
+   octet1 = 240 + (rand() % ( 240 - 255 + 1 ));
+   
+   else
+   {
+   
+   printf("\n\nPlease enter a valid class!!");
+   exit(1);
+}
+   
+    int octet2 = 0 + ( rand() % ( 255 - 0 + 1 ) );
+	int octet3 = 0 + ( rand() % ( 255 - 0 + 1 ) );
+	int octet4 = 0 + ( rand() % ( 255 - 0 + 1 ) );
+	
+	 printf("\n\nHere is your IP address: ");
+	printf("%d.%d.%d.%d",octet1,octet2,octet3,octet4);
+	
+	
+}
+
+int main()
+{
+	char IPclass;
+	
+	printf("\t\t\t\t\t*****IP ADDRESS GENERATOR*****");
+	printf("\n\n What do you want to be the class of IP address?(A/B/C/D/E): ");
+	scanf("%s",&IPclass);
+	
+	
+    IPaddressgenerator(IPclass);
+    return 0;
+}
