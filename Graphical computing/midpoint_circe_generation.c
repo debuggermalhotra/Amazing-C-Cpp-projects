a1a2649e87526bf928ed5afbd5e139e49404075a
Chetan Malhotra <chetanalawaysmalhotra@hotmail.com>
2017-02-20 23:47:51 +0530

    commit

---
+++ Graphical computing/midpoint_circe_generation.c
@@ -0,0 +1,63 @@
+#include<stdio.h>  
+#include<graphics.h> 
+
+void draw_circle(int,int,int); 
+void symmetry(int,int,int,int); 
+
+void main() 
+{  
+int xc,yc,R;  
+int gd=DETECT,gm;  
+ 
+printf("Enter the center of the circle:\n");  
+printf("Xc =");  
+scanf("%d",&xc);  
+printf("Yc =");  
+scanf("%d",&yc);  
+printf("Enter the radius of the circle :");  
+scanf("%d",&R);  
+initgraph(&gd,&gm,"NULL");  
+draw_circle(xc,yc,R);  
+getchar();  
+closegraph(); 
+} 
+ 
+void draw_circle(int xc,int yc,int rad) 
+{  
+int x = 0;  
+int y = rad;  
+int p = 1-rad;  
+symmetry(x,y,xc,yc);  
+for(x=0;y>x;x++)     
+{       
+if(p<0)                    
+p += 2*x + 3;                   
+else                  
+{         
+p += 2*(x-y) + 5;                      
+y--;                 
+}               
+symmetry(x,y,xc,yc);              
+delay(50);      
+} 
+} 
+ 
+void symmetry(int x,int y,int xc,int yc) 
+{  
+putpixel(xc+x,yc-y,GREEN);  //For pixel (x,y)  
+delay(50);  
+putpixel(xc+y,yc-x, GREEN);  //For pixel (y,x)  
+delay(50);  
+putpixel(xc+y,yc+x, GREEN);  //For pixel (y,-x) 
+delay(50);  
+putpixel(xc+x,yc+y, GREEN);  //For pixel (x,-y)  
+delay(50);  
+putpixel(xc-x,yc+y, GREEN);  //For pixel (-x,-y)  
+delay(50);  
+putpixel(xc-y,yc+x, GREEN);  //For pixel (-y,-x)  
+delay(50);  
+putpixel(xc-y,yc-x, GREEN);  //For pixel (-y,x)  
+delay(50);  
+putpixel(xc-x,yc-y, GREEN);  //For pixel (-x,y)  
+delay(50); 
+}
